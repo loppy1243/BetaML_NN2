431dd51cd2122eae0d125edc7487625cd3bb31fe
Nicholas Todoroff <loppy1243@gmail.com>
2018-07-21 15:36:54 -0400

    Add @model macro\n\nLays the framework for how the Model type is to be used

--- co.jl
+++ co.jl
@@ -1,20 +1,25 @@
+module COModel
+
 using Flux
-using Flux.Tracker
-using Lazy: @>
 
-macro :(>>>)(args...)
-    quote
-        x -> @> x $(map(esc, args)...)
-    end
-end
-
-abstract type CO <: ModelType end
-
-const NODES = prod(2(GRIDSIZE.-1).+1)
-const SCALE = (XYMAX - XYMIN)./2GRIDSIZE
+using BetaML_Data
+using Model
 
 regularize(x) = reshape(x/MAX_E, GRIDSIZE, 1, :)
 
-@model co(activ, N) = Chain(regularize,
-                            CellLayer(),
-                            @Concat(identity..., PointLayer(activ, N)))
+@model CO(activ, N, ϵ, λ) =
+    Chain(regularize, CellLayer(), @Concat(identity..., PointLayer(activ, N)))
+
+#function Model.loss(m::Model{CO}, events, points)
+#    pred_dists, pred_cells, pred_rel_points = m(events)
+#    cells = mapslices(pointcell, points, 1)
+#
+#    sum(indices(points, 2)) do i
+#        pred_prob = pred_dists[pred_cells[:, i]..., i]
+#
+#        -log(ϵ + max(0, pred_prob)) + log(1+ϵ - min(1, pred_prob)) #=
+#     =# - sum(
+#    end
+#end
+
+end # module COModel
--- model.jl
+++ model.jl
@@ -1,11 +1,13 @@
+import Flux
 using Flux.Tracker: istracked, data
 
 abstract type ModelType
 
 struct Model{T<:ModelType}
     params::Vector{Any}
-    hyperparams::Dict{String}
+    hyparams::Dict{String}
 
+    Model{<:ModelType}(params, hyparams::Dict) = new(params, hyparams)
     Model{<:ModelType}(params, hyparams...) = new(params, Dict(hyparams))
 end
 
@@ -16,3 +18,43 @@ function predict(m::Model, args...)
 end
 
 loss(m::Model) = (args...,) -> loss(m, args...)
+
+param_sym(x::Symbol) = $(esc(x))
+param_sym(x::Expr) = 
+    x.head == :call && x.args[1] == :(=>) ? param_sym(x.args[2]) : param_sym(x.args[1])
+
+param_name(x::Symbol) = string(x)
+param_name(x::Expr) =
+    x.head == :call && x.args[1] == :(=>) ? x.args[3] : param_name(x.args[1])
+
+param_esc(x::Symbol) = esc(x)
+param_esc(x::Expr) =
+    x.head == :call && x.args[1] == :(=>) ? esc(x.args[2]) : esc(x)
+
+macro model(expr::Expr)
+    @assert expr.head == :function || expr.head == := && expr.args[1] isa Expr #=
+         =# && expr.args[1].head == :call
+
+    name = expr.args[1].args[1]
+    hyparams = expr.args[1].args[2:end]
+    body = expr.args[2]
+
+    hyparams_esc = map(param_esc, hyparams)
+    hyparam_syms = map(param_sym, hyparams)
+    hyparam_names = map(param_name, hyparams)
+    name_esc = esc(name)
+
+    quote
+        abstract type $name_esc <: ModelType end
+        let model=$(esc(body))
+            function Model{$name_esc}($(hyparams_esc...))
+                params = Flux.params(model)
+                hyparams = Dict(zip($hyparam_names, ($(hyparam_syms...),)))
+
+                Model{$name_esc}(params, hyparams)
+            end
+
+            (::Model{$name_esc})(args...) = model(args...)
+        end
+    end
+end
