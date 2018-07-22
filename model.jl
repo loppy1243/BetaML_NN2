cdcb17eb1ac9efd79636eac56a1294bfa5f3b53f
Nicholas Todoroff <loppy1243@gmail.com>
2018-07-22 00:28:45 -0400

    @model works as intended
    However, intent was not good.

--- model.jl
+++ model.jl
@@ -1,15 +1,21 @@
+module ModelMod
+export Model, ModelType, predict, loss, @model
+
 import Flux
 using Flux.Tracker: istracked, data
 
-abstract type ModelType
+abstract type ModelType end
 
 struct Model{T<:ModelType}
     params::Vector{Any}
     hyparams::Dict{String}
 
-    Model{<:ModelType}(params, hyparams::Dict) = new(params, hyparams)
-    Model{<:ModelType}(params, hyparams...) = new(params, Dict(hyparams))
+    Model{T}(params, hyparams::Dict) where T = new(params, hyparams)
+    Model{T}(params, hyparams...) where T = new(params, Dict(hyparams))
 end
+
+Flux.params(m::Model) = m.params
+hyparams(m::Model) = m.hyparams
 
 predict(m::Model) = (args...,) -> predict(m, args...)
 function predict(m::Model, args...)
@@ -19,29 +25,66 @@ end
 
 loss(m::Model) = (args...,) -> loss(m, args...)
 
-param_sym(x::Symbol) = $(esc(x))
+is_pair_expr(x::Expr) = x.head == :call && x.args[1] == :(=>)
+is_pair_expr(x) = false
+
+is_kw_expr(x::Expr) = x.head == :kw
+is_kw_expr(x) = false
+
+is_param_expr(x::Expr) = x.head == :parameters
+is_param_expr(x) = false
+
+param_sym(x::Symbol) = esc(x)
 param_sym(x::Expr) = 
-    x.head == :call && x.args[1] == :(=>) ? param_sym(x.args[2]) : param_sym(x.args[1])
+    if is_pair_expr(x)
+        param_sym(x.args[2])
+    elseif is_kw_expr(x)
+        param_sym(x.args[1])
+    else
+        param_sym(x.args[1])
+    end
 
 param_name(x::Symbol) = string(x)
 param_name(x::Expr) =
-    x.head == :call && x.args[1] == :(=>) ? x.args[3] : param_name(x.args[1])
+    if is_pair_expr(x)
+        x.args[3]
+    elseif is_kw_expr(x)
+        param_name(x.args[1])
+    else
+        param_name(x.args[1])
+    end
 
 param_esc(x::Symbol) = esc(x)
 param_esc(x::Expr) =
-    x.head == :call && x.args[1] == :(=>) ? esc(x.args[2]) : esc(x)
+    if is_pair_expr(x)
+        esc(x.args[2])
+    elseif is_kw_expr(x)
+        Expr(:kw, param_esc(x.args[1]), esc(x.args[2]))
+    else
+        esc(x)
+    end
 
+# TODO: Fix a=>b syntax so it works correctly
 macro model(expr::Expr)
-    @assert expr.head == :function || expr.head == := && expr.args[1] isa Expr #=
+    @assert expr.head == :function || expr.head == :(=) && expr.args[1] isa Expr #=
          =# && expr.args[1].head == :call
 
     name = expr.args[1].args[1]
     hyparams = expr.args[1].args[2:end]
     body = expr.args[2]
 
-    hyparams_esc = map(param_esc, hyparams)
-    hyparam_syms = map(param_sym, hyparams)
-    hyparam_names = map(param_name, hyparams)
+    kwparams = if is_param_expr(hyparams[1])
+        @assert all(is_kw_expr, hyparams[1].args)
+        x = hyparams[1].args
+        hyparams = hyparams[2:end]
+        x
+    else
+        []
+    end
+
+    hyparams_esc  = [Expr(:parameters, map(param_esc, kwparams)...); map(param_esc,  hyparams)]
+    hyparam_syms  = [map(param_sym, kwparams)                      ; map(param_sym,  hyparams)]
+    hyparam_names = [map(param_name, kwparams)                     ; map(param_name, hyparams)]
     name_esc = esc(name)
 
     quote
@@ -58,3 +101,5 @@ macro model(expr::Expr)
         end
     end
 end
+
+end # module ModelMod
