56002a09864fae12e05416dfe2f0f66b1164769c
Nicholas Todoroff <loppy1243@gmail.com>
2018-07-24 22:28:09 -0400

    Export Layers and COModel from BetaML_NN

--- BetaML_NN.jl
+++ BetaML_NN.jl
@@ -3,8 +3,8 @@ module BetaML_NN
 using Reexport: @reexport
 
 include("ModelMod/main.jl")
-include("Layers/main.jl")
-include("COModel.jl")
+include("Layers/main.jl"); export Layers
+include("COModel.jl"); export COModel
 include("Tests.jl")
 
 end # module BetaML_NN
